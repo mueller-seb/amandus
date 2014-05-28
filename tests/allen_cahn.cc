6e29535585850d95ec65b53a6226753713842aa2
Guido Kanschat <dr.guido.kanschat@gmail.com>
2014-05-28 08:44:41 +0200

    more fun initial data for Allen-Cahn

--- tests/allen_cahn.cc
+++ tests/allen_cahn.cc
@@ -27,22 +27,85 @@
 #include <allen_cahn/matrix.h>
 
 
+template <int dim>
+class Startup : public dealii::Function<dim>
+{
+  public:
+    Startup();
+    virtual void value_list (const std::vector<Point<dim> > &points,
+			     std::vector<double>   &values,
+			     const unsigned int component = 0) const;
+    virtual void vector_value_list (const std::vector<Point<dim> > &points,
+				    std::vector<Vector<double> >   &values) const;
+};
+
+
+template <int dim>
+Startup<dim>::Startup ()
+		:
+		Function<dim> (1)
+{}
+
+
+template <int dim>
+void
+Startup<dim>::vector_value_list (
+  const std::vector<Point<dim> > &points,
+  std::vector<Vector<double> >   &values) const
+{
+  AssertDimension(points.size(), values.size());
+  
+  for (unsigned int k=0;k<points.size();++k)
+    {
+      const Point<dim>& p = points[k];
+      if (std::fabs(p(0)) < .8 && std::fabs(p(1)) < .2)
+	values[k](0) = 1.;
+      else if (std::fabs(p(1)) < .8 && std::fabs(p(0)) < .2)
+	values[k](0) = 1.;
+      else
+	values[k](0) = -1.;
+    }
+}
+
+
+template <int dim>
+void
+Startup<dim>::value_list (
+  const std::vector<Point<dim> > &points,
+  std::vector<double>   &values,
+  const unsigned int) const
+{
+  AssertDimension(points.size(), values.size());
+  
+  for (unsigned int k=0;k<points.size();++k)
+    {
+      const Point<dim>& p = points[k];
+      if (std::fabs(p(0)) < .8 && std::fabs(p(1)) < .2)
+	values[k] = 1.;
+      else if (std::fabs(p(1)) < .8 && std::fabs(p(0)) < .2)
+	values[k] = 1.;
+      else
+	values[k] = -1.;
+    }
+}
+
+
 int main()
 {
   const unsigned int d=2;
   
   std::ofstream logfile("deallog");
   deallog.attach(logfile);
-//  deallog.depth_console(2);
+  deallog.depth_console(2);
   
   Triangulation<d> tr;
-  GridGenerator::hyper_cube (tr, -2, 2);
-  tr.refine_global(6);
+  GridGenerator::hyper_cube (tr, -1, 1);
+  tr.refine_global(5);
   
   const unsigned int degree = 1;
   FE_DGQ<d> fe(degree);
 
-  const double diffusion = 1.e-2;
+  const double diffusion = 2.e-3;
   AllenCahn::Matrix<d> matrix_integrator(diffusion);
   AllenCahn::ExplicitResidual<d> explicit_integrator(diffusion);
   explicit_integrator.input_vector_names.push_back("Previous iterate");
@@ -67,8 +130,8 @@ int main()
   Algorithms::ThetaTimestepping<Vector<double> > timestepping(expl, newton);
   timestepping.set_output(newout);
   timestepping.theta(.55);
-  timestepping.timestep_control().start_step(.1);
-  timestepping.timestep_control().final(10.);
+  timestepping.timestep_control().start_step(.2);
+  timestepping.timestep_control().final(20.);
 
   // Now we prepare for the actual timestepping
   
@@ -77,11 +140,11 @@ int main()
   app.setup_system();
   app.setup_vector(solution);
   
-  Functions::CosineFunction<d> cosine;
-  VectorTools::interpolate(app.dof_handler, cosine, solution);
+  Startup<d> startup;
+  VectorTools::interpolate(app.dof_handler, startup, solution);
   
   dealii::AnyData indata;
   indata.add(&solution, "solution");
   dealii::AnyData outdata;
-  timestepping(indata, outdata);  
+  timestepping(indata, outdata);
 }
