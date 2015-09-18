059cce6a185a8fe780dc36bc53cf11b442e58fd1
Guido Kanschat <dr.guido.kanschat@gmail.com>
2015-09-18 10:27:34 +0200

    add image of Amandus

--- doc/main.h
+++ doc/main.h
@@ -1,8 +1,21 @@
 /**
- * @mainpage
+ * \mainpage
+ *
+ *\htmlonly
+ * <div style="float:left"><a title="By Unk (http://www.math.ru/history/people/Schwarz)
+ * [Public domain], via Wikimedia Commons"
+ * href="https://commons.wikimedia.org/wiki/File%3AKarl_Hermann_Amandus_Schwarz.jpg"><img
+ * width="256" alt="Karl Hermann Amandus Schwarz"
+ * src="https://upload.wikimedia.org/wikipedia/commons/e/ea/Karl_Hermann_Amandus_Schwarz.jpg"/></a></div>
+ * \endhtmlonly
  *
  * Amandus is a platform for solving mixed finite element
- * problems. The solvers are based on multilevel overlapping Schwarz
+ * problems. It is named after Kerl Hermann Amandus Schwarz, who
+ * invented the alternating Schwarz method and whose name is
+ * commemorated in the inequality named after him, Bunyakovsky, and
+ * Cauchy in varying combinations.
+ *
+ * The solvers are based on multilevel overlapping Schwarz
  * methods. Ideally, a program written with the help of Amandus
  * consists only of a short driver file and functions which integrate
  * a residual and a matrix on cells and faces of a mesh.
