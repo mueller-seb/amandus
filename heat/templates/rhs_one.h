af3d5d1441c5188f3389170362c2f8c726fb3b2a
Sebastian Müller <mueller.sbstn@gmail.com>
2018-11-17 22:49:28 +0100

    templates updated

--- heat/templates/rhs_one.h
+++ heat/templates/rhs_one.h
@@ -4,8 +4,8 @@
  *
  * See the files AUTHORS and LICENSE in the project root directory
  **********************************************************************/
-#ifndef __heat_rhs_one_h
-#define __heat_rhs_one_h
+#ifndef __rhs_one_h
+#define __rhs_one_h
 
 #include <deal.II/meshworker/integration_info.h>
 #include <deal.II/meshworker/local_integrator.h>
@@ -16,28 +16,29 @@ using namespace dealii;
 using namespace LocalIntegrators;
 
 /**
+ * \brief Very simple local integrator for constant right hand side.
+ *
  * A local integrator for a right hand side equal to constant one in
- * the vector valued equation and zero in the scalar.
+ * the scalar case and equal to the vector with constant one in every
+ * entry, respectively.
  */
-namespace HeatIntegrators
-{
 template <int dim>
 class RhsOne : public AmandusIntegrator<dim>
 {
+  const unsigned int components;
 public:
-  RhsOne();
-  virtual void cell(MeshWorker::DoFInfo<dim>& dinfo, MeshWorker::IntegrationInfo<dim>& info) const;
+  /// Constructor, with argument system dimension.
+  RhsOne(const unsigned int components=dim)
+    :components(components)
+  {}
+  
+  virtual void cell(MeshWorker::DoFInfo<dim>& dinfo, MeshWorker::IntegrationInfo<dim>& info) const override;
   virtual void boundary(MeshWorker::DoFInfo<dim>& dinfo,
-                        MeshWorker::IntegrationInfo<dim>& info) const;
+                        MeshWorker::IntegrationInfo<dim>& info) const override;
   virtual void face(MeshWorker::DoFInfo<dim>& dinfo1, MeshWorker::DoFInfo<dim>& dinfo2,
                     MeshWorker::IntegrationInfo<dim>& info1,
-                    MeshWorker::IntegrationInfo<dim>& info2) const;
+                    MeshWorker::IntegrationInfo<dim>& info2) const override;
 };
-
-template <int dim>
-RhsOne<dim>::RhsOne()
-{
-}
 
 template <int dim>
 void
@@ -48,7 +49,7 @@ RhsOne<dim>::cell(MeshWorker::DoFInfo<dim>& dinfo, MeshWorker::IntegrationInfo<d
   Vector<double>& result = dinfo.vector(0).block(0);
   for (unsigned int k = 0; k < fe.n_quadrature_points; ++k)
     for (unsigned int i = 0; i < n_dofs; ++i)
-      for (unsigned int d = 0; d < dim; ++d)
+      for (unsigned int d = 0; d < components; ++d)
         result(i) += fe.JxW(k) * fe.shape_value_component(i, k, d);
 }
 
@@ -66,5 +67,5 @@ RhsOne<dim>::face(MeshWorker::DoFInfo<dim>& /*dinfo1*/, MeshWorker::DoFInfo<dim>
                   MeshWorker::IntegrationInfo<dim>& /*info2*/) const
 {
 }
-}
+
 #endif
